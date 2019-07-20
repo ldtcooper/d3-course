913e1b696c6e0f795bfb2e47dd34632551fcc227
Logan Cooper <ldtcoop@gmail.com>
2019-07-20 10:42:29 -0700

    Finish bar chart section

--- 4-1-starter/app.js
+++ 4-1-starter/app.js
@@ -1,6 +1,33 @@
-var data = [10,20,30,40,50];
+const data = [];
+const chartWidth = 800;
+const chartHeight = 400;
+const barPadding = 5;
 
-d3.select('#chart')
-    .selectAll('rect')
+for (let i = 0; i < 5; i++) {
+    const num = d3.randomUniform(1,50)
+    data.push(Math.floor(num()));
+}
+
+const svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', chartWidth)
+    .attr('height', chartHeight);
+
+svg.selectAll('rect')
     .data(data)
-    .enter();
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * (chartWidth / data.length))
+    .attr('y', (d) => chartHeight - (d * 5))
+    .attr('width', (chartWidth / data.length) - barPadding)
+    .attr('height', (d) => d * 5)
+    .attr('fill', 'green');
+
+svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text((d) => d)
+    .attr('x', (d, i) => i * (chartWidth / data.length) + (chartWidth / data.length - barPadding) / 2)
+    .attr('y', (d) => chartHeight - (d * 5) - 2)
+    .attr('text-anchor', 'middle')
